b4fd2b96a0f332b10329d4edf4ab3a3d2a1aee1a
Eren Köse <116944944+erennkose@users.noreply.github.com>
2024-11-28 21:13:49 +0300

    Update polarite.py

--- polarite.py
+++ polarite.py
@@ -1,4 +1,10 @@
 from random import randint
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+etkisiz_kelimeler = set(stopwords.words('turkish')) # Türkçedeki etkisiz (zamir vs.) kelimeler
 
 def calculatePolarite(sent:str) -> bool:
+    kelimeler = [kelime for kelime in word_tokenize(sent) if kelime.lower() not in etkisiz_kelimeler] # Filtrelenmiş metnin listeye dönüştürülmüş hali
+    print("Filtered words:", kelimeler)
     return bool(randint(0,1))
