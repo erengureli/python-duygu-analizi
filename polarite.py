008c3f7e2ed918d20a86bf91ebc100fc10bf8b37
Eren GURELI <erengureli10@gmail.com>
2024-11-28 21:19:04 +0300

    Update

--- polarite.py
+++ polarite.py
@@ -2,6 +2,11 @@ from random import randint
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
+# 1 kere bunları indirdikten sonra bir daha indirmeye gerek kalmıyor
+# from nltk import download
+# download('stopwords')
+# download('punkt_tab')
+
 etkisiz_kelimeler = set(stopwords.words('turkish')) # Türkçedeki etkisiz (zamir vs.) kelimeler
 
 def calculatePolarite(sent:str) -> bool:
