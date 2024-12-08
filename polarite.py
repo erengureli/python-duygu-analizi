4c5f16fb4390ab7eb458ec354847f59501a43c79
Eren Köse <erenkosee471@gmail.com>
2024-12-08 16:29:49 +0300

    Update polarite.py

--- polarite.py
+++ polarite.py
@@ -26,33 +26,7 @@ negatif_ekler = {"memez","mamaz", "maz", "mez", "mıyor", "miyor", "ma", "me"}
 yalanci_negatif = {"mak", "mek", "malı", "meli"}
 punction = {".", ",", "!", "?", ":", "...", ";", "-", "\'"}
 
-def calculatePolarite(sentence: str) -> str:
-    kelimeler = [kelime.lower() for kelime in word_tokenize(sentence) if kelime.lower() not in etkisiz_kelimeler]
-    print(kelimeler)
-
-    olumsuzluk_var = False
-
-    pozitif_sayisi = sum(1 for kelime in kelimeler if kelime in pozitif_kelimeler)
-    negatif_sayisi = sum(1 for kelime in kelimeler if kelime in negatif_kelimeler)
-
-    # Olumsuzluk eklerini tespit et
-    for kelime in kelimeler: 
-        for ek in negatif_ekler:
-            if search(ek,kelime):
-                olumsuzluk_var = True
-
-    # Olumsuzluk ekleri varsa negatif kabul et
-    if pozitif_sayisi == 0 and negatif_sayisi == 0 and olumsuzluk_var:
-        return False  
-
-    # Değil varsa negatif kabul et (değil mi? gibi olan yerlerde ise değilin negatif olarak kullanılmadığını belirt)
-    if "değil" in kelimeler and not("?" in sentence or "mi" in kelimeler):
-        return False  
-
-    return True if pozitif_sayisi >= negatif_sayisi else False
-
-
-def calculatePolarite2(paragraph: str) -> bool:
+def calculatePolarite(paragraph: str) -> bool:
     words = [word.lower() for word in word_tokenize(paragraph) if word.lower() not in etkisiz_kelimeler]
 
     newKelimeler = []
@@ -100,4 +74,4 @@ def puncRmv(liste: list):
             if pn in eleman[0]:
                 liste.remove(eleman)
 
-calculatePolarite2("O buraya gelmemezlik yapmazdı.")
+calculatePolarite("O buraya gelmemezlik yapmazdı.")
