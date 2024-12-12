e11aec451ecab3ac14278c942aa6faa56bcd9379
Eren GURELI <erengureli10@gmail.com>
2024-12-12 10:41:06 +0300

    update words

--- words.py
+++ words.py
@@ -2,6 +2,8 @@ from nltk.corpus import stopwords
 
 etkisiz_kelimeler = set(stopwords.words('turkish'))
 
+onemli_baglac = {"ama", "fakat"}
+
 # pozitif_kelimeler = {"mutlu", "güzel", "iyi", "harika", "başarılı", "pozitif", "mükemmel"}
 
 negatif_kelimeler = {"üzgün", "kötü", "berbat", "korkunç", "negatif", "değil","sorun",
@@ -10,7 +12,7 @@ negatif_kelimeler = {"üzgün", "kötü", "berbat", "korkunç", "negatif", "değ
                      "solmak", "tehlike", "boşuna", "hüzün", "kayıp", "yoksunmak", "kırık",
                      "boğmak","sinir","yok","çirkin","üzülmek","nefret","yalan",
                      "açgözlülük","cimri","iğrenç","zehir","ölü","yaralı","tiksinmek",
-                     "mızmız","vefat","mutsuz"}
+                     "mızmız","vefat","mutsuz","gergin"}
 
 yalanci_negatif = { "malı", "meli","mayı","meyi", "maya", "meye", "ması","mesi","mak", "mek"}
 
